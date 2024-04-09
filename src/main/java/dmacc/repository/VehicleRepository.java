64f8e750bf809a17b6fadb5bd6b5d5aa094e8c7c
Ethan-Tran-115 <156397509+Ethan-Tran-115@users.noreply.github.com>
2024-04-08 21:32:22 -0500

    create customer and vehicle entities

--- src/main/java/dmacc/repository/VehicleRepository.java
+++ src/main/java/dmacc/repository/VehicleRepository.java
@@ -1,11 +1,12 @@
 package dmacc.repository;
-import dmacc.beans.Customer;
 import org.springframework.data.jpa.repository.JpaRepository;
+
+import dmacc.beans.Vehicle;
 /**
  * @author Phuoc Tran - ptran9@dmacc.edu
  * CIS175 - Spring 2024
  * Apr 8, 2024
  */
-public interface CustomerRepository extends JpaRepository<Customer, Long> {
+public interface VehicleRepository extends JpaRepository<Vehicle, Integer> {
 
 }
