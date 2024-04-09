29321b7622904f02652476477fcfc21aed0619d5
daj0606 <127715000+daj0606@users.noreply.github.com>
2024-04-09 18:43:39 -0500

    Fixed small bugs in webcontroller

--- src/main/java/dmacc/controller/WebController.java
+++ src/main/java/dmacc/controller/WebController.java
@@ -54,20 +54,20 @@ public class WebController {
 		return viewAllCustomers(model);
 	}
 
-	@GetMapping("/edit/{id}")
+	@GetMapping("/editCustomer/{id}")
 	public String showUpdateCustomer(@PathVariable("id") long id, Model model) {
 		Customer c = customerRepository.findById(id).orElse(null);
 		model.addAttribute("newCustomer", c);
 		return "input";
 	}
 
-	@PostMapping("/update/{id}")
+	@PostMapping("/updateCustomer/{id}")
 	public String reviseCustomer(Customer c, Model model) {
 		customerRepository.save(c);
 		return viewAllCustomers(model);
 	}
 
-	@GetMapping("/delete/{id}")
+	@GetMapping("/deleteCustomer/{id}")
 	public String deleteCustomer(@PathVariable("id") long id, Model model) {
 		Customer c = customerRepository.findById(id).orElse(null);
 		customerRepository.delete(c);
@@ -87,20 +87,20 @@ public class WebController {
 		return viewAllCustomers(model);
 	}
 
-	@GetMapping("/edit/{id}")
+	@GetMapping("/editVehicle/{id}")
 	public String showUpdateVehicle(@PathVariable("id") long id, Model model) {
 		Vehicle v = vehicleRepository.findById(id).orElse(null);
 		model.addAttribute("newVehicle", v);
 		return "input";
 	}
 
-	@PostMapping("/update/{id}")
+	@PostMapping("/updateVehicle/{id}")
 	public String reviseVehicle(Vehicle v, Model model) {
 		vehicleRepository.save(v);
 		return viewAllCustomers(model);
 	}
 
-	@GetMapping("/delete/{id}")
+	@GetMapping("/deleteVehicle/{id}")
 	public String deleteVehicle(@PathVariable("id") long id, Model model) {
 		Vehicle v = vehicleRepository.findById(id).orElse(null);
 		vehicleRepository.delete(v);
@@ -116,7 +116,7 @@ public class WebController {
 		return "results";
 	}
 
-	@GetMapping("/inputCustomer")
+	@GetMapping("/inputAppointment")
 	public String addNewAppointment(Model model) {
 		Appointment a = new Appointment();
 		model.addAttribute("newCustomer", a);
@@ -129,20 +129,20 @@ public class WebController {
 		return viewAllAppointments(model);
 	}
 
-	@GetMapping("/edit/{id}")
+	@GetMapping("/editAppointment/{id}")
 	public String showUpdateAppointment(@PathVariable("id") long id, Model model) {
 		Appointment a = appointmentRepository.findById(id).orElse(null);
 		model.addAttribute("newAppointment", a);
 		return "input";
 	}
 
-	@PostMapping("/update/{id}")
+	@PostMapping("/updateAppointment/{id}")
 	public String reviseAppointment(Appointment a, Model model) {
 		appointmentRepository.save(a);
 		return viewAllAppointments(model);
 	}
 
-	@GetMapping("/delete/{id}")
+	@GetMapping("/deleteAppointment/{id}")
 	public String deleteAppointment(@PathVariable("id") long id, Model model) {
 		Appointment a = appointmentRepository.findById(id).orElse(null);
 		appointmentRepository.delete(a);
